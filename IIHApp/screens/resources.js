17ec4dbe24e6ba4ba16e16eb70b5dbca04394661
terrimk <tkellerman16@gmail.com>
2021-02-07 20:47:24 -0500

    starting flatlist in resources.js

--- IIHApp/screens/resources.js
+++ IIHApp/screens/resources.js
@@ -9,16 +9,24 @@
  * By: Terri Kellerman, February 2021
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import {
   View,
   Text,
   Button,
   StyleSheet,
   Alert,
-  Linking
+  Linking,
+  FlatList
 } from 'react-native';
 import FlatButton from '../shared/button';
+
+const [url, setURL] = useState ([
+  {name: 'We Are Rare Gems', url: 'https://www.weareraregems.com', key: '1' },
+  {name: 'WebMD', url: 'https://www.webmd.com/brain/pseudotumor-cerebri#1', key: '2' },
+  {name: 'NIH', url: 'https://www.nei.nih.gov/learn-about-eye-health/eye-conditions-and-diseases/idiopathic-intracranial-hypertension', key: '3' },
+  {name: 'NORD', url: 'https://rarediseases.org/rare-diseases/idiopathic-intracranial-hypertension/', key: '4' }, 
+]);
 
 const raregemsURL = "https://www.weareraregems.com";
 const webmdURL = "https://www.webmd.com/brain/pseudotumor-cerebri#1";
@@ -27,18 +35,32 @@ const nordURL = "https://rarediseases.org/rare-diseases/idiopathic-intracranial-
 
 export default function Resources () {
     return (
-      <View style={styles.pageView}>
-        <Text style={styles.title}>Resources</Text>
-            <View style={styles.buttonView}>  
-              <FlatButton style={styles.button1} text='We Are Rare Gems' onPress={() => Linking.openURL(raregemsURL)} />
-              <FlatButton text='WebMD' onPress={() => Linking.openURL(webmdURL)} /> 
-            </View>
-            <View style={styles.buttonView}> 
-              <FlatButton text='NIH' onPress={() => Linking.openURL(nihURL)} /> 
-              <FlatButton text='NORD' onPress={() => Linking.openURL(nordURL)} /> 
-            </View>
-      </View>
+      <Text style={styles.title}>Resources</Text>
+
+      <FlatList
+        data={raregemURL}
+      />
+
     )
+
+      // FIRST ATTEMPT AT LINK DISPLAY START
+      // <View style={styles.pageView}>
+      //   <Text style={styles.title}>Resources</Text>
+      //       <View style={styles.buttonView}>  
+      //         <FlatButton style={styles.button1} text='We Are Rare Gems' onPress={() => Linking.openURL(raregemsURL)} />
+      //       {/* </View>  
+      //       <View style={styles.buttonView}>  */}
+      //           <FlatButton style={styles.button2} text='WebMD' onPress={() => Linking.openURL(webmdURL)} /> 
+      //       </View>
+      //       <View style={styles.buttonView}> 
+      //         <FlatButton text='NIH' onPress={() => Linking.openURL(nihURL)} />
+      //       {/* </View> 
+      //       <View style={styles.buttonView}>  */}
+      //         <FlatButton text='NORD' onPress={() => Linking.openURL(nordURL)} /> 
+      //       </View>
+      // </View>
+      // FIRST ATTEMPT AT LINK DISPLAY END
+    
   }
 
   const styles = StyleSheet.create ({
@@ -46,8 +68,7 @@ export default function Resources () {
       flex: 1,
       flexDirection: 'column',  // top to bottom orientation (this is default)
       alignItems: 'center',  // works in cross axis direction, in this case will be centered vertically
-      justifyContent: 'flex-start', // works in axis direction, in this case will be centered horizontally
-      flexWrap: 'wrap',
+      justifyContent: 'center', // works in axis direction, in this case will be centered horizontally
       backgroundColor: 'snow',
     },
     buttonView: {
@@ -55,9 +76,14 @@ export default function Resources () {
       flexDirection: 'row',
       justifyContent: 'space-around',
       alignItems: 'center',
+      marginRight: 20,
     },
     button1:{
+      alignSelf: 'flex-start',
+    },
+    button2:{
       alignSelf: 'flex-end',
+      backgroundColor: '#f05d91',
     },
     title: {
       fontSize: 30,
