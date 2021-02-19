1dff2ee3708787613073a0b75d05191323e85a5a
terrimk <tkellerman16@gmail.com>
2021-02-18 19:49:36 -0500

    installed react nav/stack

--- IIHApp/App.js
+++ IIHApp/App.js
@@ -23,36 +23,48 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Header from './shared/header';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import { getFocusedRouteNameFromRoute } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-function getHeaderTitle(route) {
-  // If the focused route is not found, we need to assume it's the initial screen
-  // This can happen during if there hasn't been any navigation inside the screen
-  // In our case, it's "Feed" as that's the first screen inside the navigator
-  const routeName = getFocusedRouteNameFromRoute(route) ?? 'Home';
+const Tab = createBottomTabNavigator();
+const HomeStack = createStackNavigator();
+const SettingsStack = createStackNavigator();
 
-  switch (routeName) {
-    case 'Home':
-      return 'Home';
-    case 'Community':
-      return 'Community';
-    case 'Resources':
-      return 'Resources';
-    case 'About':
-      return 'You Are Not Alone.';
-  }
+
+function HomeStackScreen() {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="Home" component={Home} />
+    </HomeStack.Navigator>
+  );
 }
 
-const Tab = createBottomTabNavigator();
-
-function HomeTabs({ navigation, route }) {
-  React.useLayoutEffect(() => {
-    navigation.setOptions({ headerTitle: getHeaderTitle(route) });
-  }, [navigation, route]);
-
+function CommunityStackScreen() {
   return (
-            <Tab.Navigator
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="Community" component={Community} />
+    </HomeStack.Navigator>
+  );
+}
+function ResourcesStackScreen() {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="Resources" component={Resources} />
+    </HomeStack.Navigator>
+  );
+}
+function AboutStackScreen() {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="You Are Not Alone." component={About} />
+    </HomeStack.Navigator>
+  );
+}
+
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
             screenOptions={({ route }) => ({
               tabBarIcon: ({ focused, color, size }) => {
                 let iconName;
@@ -80,60 +92,9 @@ function HomeTabs({ navigation, route }) {
         <Tab.Screen name="Resources" component={Resources} />
         <Tab.Screen name="About" component={About} />
         </Tab.Navigator>
-        </NavigationContainer>
-          );
-}
-
-const Stack = createStackNavigator();
-
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Community" component={Community} />
-        <Stack.Screen name="Resources" component={Resources} />
-        <Stack.Screen name="About" component={About} />
-      </Stack.Navigator>
     </NavigationContainer>
   );
 }
-//ORIGINAL TAB NAV
-// //const App: () => React$Node = () => {
-//   export default function App() {
-//   return (
-//     <NavigationContainer>
-//       <Tab.Navigator
-//               screenOptions={({ route }) => ({
-//                 tabBarIcon: ({ focused, color, size }) => {
-//                   let iconName;
 
-//                   if (route.name === 'Home') {
-//                     iconName = focused ? 'home' : 'home-outline';
-//                   } else if (route.name === 'Community') {
-//                     iconName = focused ? 'people-circle' : 'people-circle-outline';
-//                   } else if (route.name === 'Resources') {
-//                     iconName = focused ? 'information-circle' : 'information-circle-outline';
-//                   } else if (route.name === 'About') {
-//                     iconName = focused ? 'heart-circle' : 'heart-circle-outline';
-//                   }
-//                  // You can return any component that you like here!
-//             return <Ionicons name={iconName} size={size} color={color} />;
-//           },
-//         })}
-//         tabBarOptions={{
-//           activeTintColor: 'cornflowerblue',
-//           inactiveTintColor: 'gray',
-//         }}
-//       >
-//         <Tab.Screen name="Home" component={Home}  />
-//         <Tab.Screen name="Community" component={Community} />
-//         <Tab.Screen name="Resources" component={Resources} />
-//         <Tab.Screen name="About" component={About} />
-//       </Tab.Navigator>
-//     </NavigationContainer>
-//   )
-// }
 
-//export default App;
 
